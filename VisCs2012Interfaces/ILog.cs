c2f54eee01fc30e7aa349ebf60ad6013d0323b6e
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2018-04-06 20:40:18 +0200

    Add project files.

---
+++ VisCs2012Interfaces/ILog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisCs2012Interfaces
+{
+    interface ILog
+    {
+        void Write(String msg);
+        string Target
+        {
+            set;
+            get;
+        }
+    }
+}
---
+++ VisCs2012Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisCs2012Interfaces
+{
+    public class TextLog : ILog
+    {
+        public void Write(string msg)
+        {
+            FileInfo fi = new FileInfo(_target);
+            StreamWriter sw = fi.AppendText();
+            sw.WriteLine(DateTime.Now + " "+ msg);
+            sw.Close();
+        }
+
+        private string _target;
+        public string Target
+        {
+            get { return _target;}
+            set { _target = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILog log;
+            log = new TextLog();
+            log.Target = "c:\\temp\\test.log";
+            log.Write("tekstlogger via interface");
+        }
+    }
+}
